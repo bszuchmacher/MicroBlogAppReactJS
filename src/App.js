b6a54f8ad661e20028648844dcf4ec6b0e109ed7
Ben Szuchmacher <59138522+bszuchmacher@users.noreply.github.com>
2020-08-02 14:04:02 -0400

    Milestone 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import TweetsList from './Components/TweetsList';
+import TweetsMainPage from './Components/TweetsMainPage';
+import ProfilePage from './Components/ProfilePage';
+import NavBar from './Components/NavBar';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: 'Bunty Jones',
+    }
+    this.currentUserName = this.currentUserName.bind(this)
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <TweetsList />
-    </div>
-  );
+  componentDidMount() {
+    const savedName = localStorage.getItem('savedName');
+    const heldName = JSON.parse(savedName);
+    if (savedName) {
+      this.setState(() => ({ userName: heldName }))
+    } else {
+      this.setState(() => ({ userName: this.state.userName }))
+    }
+  }
+
+  componentDidUpdate() {
+    const local = this.state.userName;
+    localStorage.setItem('savedName', JSON.stringify(local))
+  }
+
+  currentUserName = (newName) => {
+    this.setState({ userName: newName });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact>
+              <TweetsMainPage currentUserName={this.state.userName} />
+            </Route>
+            <Route path="/profile" exact>
+              <ProfilePage currentUserName={this.state.userName} updatedUserName={this.currentUserName} />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
 export default App;
